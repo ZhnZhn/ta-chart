c15fad35c1518b10599dd68acb58a07a9586e24f
Yevhenii Molodyi <eugmolody@gmail.com>
2022-02-15 11:13:08 +0200

    Replace useInit implementation

--- src/components/hooks/useInit.js
+++ src/components/hooks/useInit.js
@@ -1,8 +1,8 @@
+import { useRef } from '../uiApi';
 
-let obj;
-
-const useInit = (fn) => obj
-  ? obj
-  : (obj = fn())  
+const useInit = fn => {
+  const _ref = useRef();
+  return _ref.current || (_ref.current = fn());
+};
 
 export default useInit
