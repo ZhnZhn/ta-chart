cafd0e5de26cb41287718c518777cfac02f8d7e5
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-03 12:55:25 +0300

    Replace util functions by arrow functions

--- js/components/zhn-charts/utils/index.js
+++ js/components/zhn-charts/utils/index.js
@@ -3,7 +3,8 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
-exports.zipper = exports.slidingWindow = exports.rebind = exports.path = exports.overlayColors = exports.merge = exports.identity = exports.functor = void 0;
+exports.find = find;
+exports.zipper = exports.slidingWindow = exports.rebind = exports.plotDataLengthBarWidth = exports.path = exports.overlayColors = exports.merge = exports.last = exports.isNotDefined = exports.isDefined = exports.identity = exports.hexToRGBA = exports.head = exports.getStrokeDasharrayCanvas = exports.getStrokeDasharray = exports.functor = exports.first = void 0;
 
 var _d3Scale = require("d3-scale");
 
@@ -38,4 +39,176 @@ var schemeCategory10 = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "
 
 var overlayColors = (0, _d3Scale.scaleOrdinal)(schemeCategory10);
 exports.overlayColors = overlayColors;
+
+var isDefined = function isDefined(d) {
+  return d !== null && typeof d != "undefined";
+};
+
+exports.isDefined = isDefined;
+
+var isNotDefined = function isNotDefined(d) {
+  return !isDefined(d);
+};
+
+exports.isNotDefined = isNotDefined;
+
+function find(list, predicate, context) {
+  if (context === void 0) {
+    context = this;
+  }
+
+  for (var i = 0; i < list.length; ++i) {
+    if (predicate.call(context, list[i], i, list)) {
+      return list[i];
+    }
+  }
+
+  return;
+}
+
+var hexToRGBA = function hexToRGBA(inputHex, opacity) {
+  var hex = inputHex.replace("#", "");
+
+  if (inputHex.indexOf("#") > -1 && (hex.length === 3 || hex.length === 6)) {
+    var multiplier = hex.length === 3 ? 1 : 2,
+        r = parseInt(hex.substring(0, 1 * multiplier), 16),
+        g = parseInt(hex.substring(1 * multiplier, 2 * multiplier), 16),
+        b = parseInt(hex.substring(2 * multiplier, 3 * multiplier), 16),
+        result = "rgba(" + r + ", " + g + ", " + b + ", " + opacity + ")";
+    return result;
+  }
+
+  return inputHex;
+};
+
+exports.hexToRGBA = hexToRGBA;
+
+var getStrokeDasharray = function getStrokeDasharray(type) {
+  switch (type) {
+    default:
+    case "Solid":
+      return "none";
+
+    case "ShortDash":
+      return "6, 2";
+
+    case "ShortDash2":
+      return "6, 3";
+
+    case "ShortDot":
+      return "2, 2";
+
+    case "ShortDashDot":
+      return "6, 2, 2, 2";
+
+    case "ShortDashDotDot":
+      return "6, 2, 2, 2, 2, 2";
+
+    case "Dot":
+      return "2, 6";
+
+    case "Dash":
+      return "8, 6";
+
+    case "LongDash":
+      return "16, 6";
+
+    case "DashDot":
+      return "8, 6, 2, 6";
+
+    case "LongDashDot":
+      return "16, 6, 2, 6";
+
+    case "LongDashDotDot":
+      return "16, 6, 2, 6, 2, 6";
+  }
+};
+
+exports.getStrokeDasharray = getStrokeDasharray;
+
+var getStrokeDasharrayCanvas = function getStrokeDasharrayCanvas(type) {
+  var a = getStrokeDasharray(type).split(",");
+  return a.length === 1 ? [] : a.map(function (d) {
+    return Number(d);
+  });
+};
+
+exports.getStrokeDasharrayCanvas = getStrokeDasharrayCanvas;
+
+var last = function last(array, accessor) {
+  if (accessor && array) {
+    var value;
+
+    for (var i = array.length - 1; i >= 0; i--) {
+      value = array[i];
+
+      if (isDefined(accessor(value))) {
+        return value;
+      }
+    }
+
+    return;
+  }
+
+  var length = array ? array.length : 0;
+  return length ? array[length - 1] : void 0;
+}; //CandlestickSeries
+
+
+exports.last = last;
+
+var head = function head(array, accessor) {
+  if (accessor && array) {
+    var value;
+
+    for (var i = 0; i < array.length; i++) {
+      value = array[i];
+
+      if (isDefined(accessor(value))) {
+        return value;
+      }
+    }
+
+    return;
+  }
+
+  return array ? array[0] : void 0;
+};
+
+exports.head = head;
+var first = head;
+/**
+ * Bar width is based on the amount of items in the plot data and the distance between the first and last of those
+ * items.
+ * @param props the props passed to the series.
+ * @param moreProps an object holding the xScale, xAccessor and plotData.
+ * @return {number} the bar width.
+ */
+
+exports.first = first;
+
+var plotDataLengthBarWidth = function plotDataLengthBarWidth(props, moreProps) {
+  var widthRatio = props.widthRatio,
+      xScale = moreProps.xScale,
+      _xScale$range = xScale.range(),
+      l = _xScale$range[0],
+      r = _xScale$range[1],
+      totalWidth = Math.abs(r - l);
+
+  var width;
+
+  if (xScale.invert != null) {
+    var _xScale$domain = xScale.domain(),
+        dl = _xScale$domain[0],
+        dr = _xScale$domain[1];
+
+    width = totalWidth / Math.abs(dl - dr);
+  } else {
+    width = totalWidth / xScale.domain().length;
+  }
+
+  return width * widthRatio;
+};
+
+exports.plotDataLengthBarWidth = plotDataLengthBarWidth;
 //# sourceMappingURL=index.js.map
--- src/components/zhn-charts/utils/index.js
+++ src/components/zhn-charts/utils/index.js
@@ -1,14 +1,161 @@
-import { scaleOrdinal } from  "d3-scale";
+import { scaleOrdinal } from  'd3-scale';
 
-export { rebind } from "./rebind";
-export { default as merge } from "./merge";
-export { default as slidingWindow } from "./slidingWindow";
-export { default as zipper } from "./zipper";
-export { default as identity } from "./identity";
-export { path } from "./path";
-export { functor } from "./functor";
+export { rebind } from './rebind';
+export { default as merge } from './merge';
+export { default as slidingWindow } from './slidingWindow';
+export { default as zipper } from './zipper';
+export { default as identity } from './identity';
+export { path } from './path';
+export { functor } from './functor';
 
-const schemeCategory10 = ["#1f77b4","#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+const schemeCategory10 = [
+  "#1f77b4",
+  "#ff7f0e",
+  "#2ca02c",
+  "#d62728",
+  "#9467bd",
+  "#8c564b",
+  "#e377c2",
+  "#7f7f7f",
+  "#bcbd22",
+  "#17becf"
+];
 //const defaultColors = ["#F44336", "#2196F3", "#8BC34A", "#FF5722", "#3F51B5", "#03A9F4", "#9C27B0", "#4CAF50"];
 
 export const overlayColors = scaleOrdinal(schemeCategory10);
+
+export const isDefined = d => d !== null
+  && typeof d != "undefined"
+
+export const isNotDefined = d => !isDefined(d)
+
+export function find(list, predicate, context = this) {
+	for (let i = 0; i < list.length; ++i) {
+		if (predicate.call(context, list[i], i, list)) {
+			return list[i];
+		}
+	}
+	return;
+}
+
+export const hexToRGBA = (
+  inputHex,
+  opacity
+) => {
+	const hex = inputHex.replace("#", "");
+	if (inputHex.indexOf("#") > -1 && (hex.length === 3 || hex.length === 6)) {
+		const multiplier = hex.length === 3 ? 1 : 2
+		, r = parseInt(hex.substring(0, 1 * multiplier), 16)
+		, g = parseInt(hex.substring(1 * multiplier, 2 * multiplier), 16)
+		, b = parseInt(hex.substring(2 * multiplier, 3 * multiplier), 16)
+
+		, result = `rgba(${ r }, ${ g }, ${ b }, ${ opacity })`;
+
+		return result;
+	}
+	return inputHex;
+}
+
+export const getStrokeDasharray = (type) => {
+	switch (type) {
+		default: case "Solid":
+			return "none";
+		case "ShortDash":
+			return "6, 2";
+		case "ShortDash2":
+			return "6, 3";
+		case "ShortDot":
+			return "2, 2";
+		case "ShortDashDot":
+			return "6, 2, 2, 2";
+		case "ShortDashDotDot":
+			return "6, 2, 2, 2, 2, 2";
+		case "Dot":
+			return "2, 6";
+		case "Dash":
+			return "8, 6";
+		case "LongDash":
+			return "16, 6";
+		case "DashDot":
+			return "8, 6, 2, 6";
+		case "LongDashDot":
+			return "16, 6, 2, 6";
+		case "LongDashDotDot":
+			return "16, 6, 2, 6, 2, 6";
+	}
+}
+
+export const getStrokeDasharrayCanvas = (
+  type
+) => {
+  const a = getStrokeDasharray(type)
+    .split(",");
+  return a.length === 1
+    ? []
+    : a.map((d) => Number(d));
+};
+
+export const last = (
+  array,
+  accessor
+) => {
+    if (accessor && array) {
+       let value;
+       for (let i = array.length - 1; i >= 0; i--) {
+          value = array[i];
+          if (isDefined(accessor(value))) {
+            return value;
+          }
+       }
+       return;
+    }
+    const length = array ? array.length : 0;
+    return length
+      ? array[length - 1]
+      : void 0;
+}
+
+//CandlestickSeries
+export const head = (
+  array,
+  accessor
+) => {
+	if (accessor && array) {
+		let value;
+		for (let i = 0; i < array.length; i++) {
+			value = array[i];
+			if (isDefined(accessor(value))) {
+        return value;
+      }
+		}
+		return;
+	}
+	return array ? array[0] : void 0;
+}
+
+export const first = head
+
+/**
+ * Bar width is based on the amount of items in the plot data and the distance between the first and last of those
+ * items.
+ * @param props the props passed to the series.
+ * @param moreProps an object holding the xScale, xAccessor and plotData.
+ * @return {number} the bar width.
+ */
+export const plotDataLengthBarWidth = (
+  props,
+  moreProps
+) => {
+	const { widthRatio } = props
+	, { xScale } = moreProps
+	, [l, r] = xScale.range()
+	, totalWidth = Math.abs(r - l);
+  let width;
+	if (xScale.invert != null) {
+		const [dl, dr] = xScale.domain();
+		width = totalWidth / Math.abs(dl - dr);
+	} else {
+		width = totalWidth / xScale.domain().length;
+	}
+  return width * widthRatio;
+}
