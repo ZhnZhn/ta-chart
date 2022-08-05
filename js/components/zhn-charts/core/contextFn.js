79e9148a338e9d824d6493cef9f4ed2a2d986d95
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-05 10:13:38 +0300

    Add contextFn

---
+++ js/components/zhn-charts/core/contextFn.js
@@ -0,0 +1,17 @@
+"use strict";
+
+exports.__esModule = true;
+exports.getMouseCanvas = exports.getAxisCanvas = void 0;
+
+var getAxisCanvas = function getAxisCanvas(contexts) {
+  return contexts.axes;
+};
+
+exports.getAxisCanvas = getAxisCanvas;
+
+var getMouseCanvas = function getMouseCanvas(contexts) {
+  return contexts.mouseCoord;
+};
+
+exports.getMouseCanvas = getMouseCanvas;
+//# sourceMappingURL=contextFn.js.map
---
+++ src/components/zhn-charts/core/contextFn.js
@@ -0,0 +1,7 @@
+export const getAxisCanvas = (
+  contexts
+) => contexts.axes;
+
+export const getMouseCanvas = (
+  contexts
+) =>  contexts.mouseCoord;
