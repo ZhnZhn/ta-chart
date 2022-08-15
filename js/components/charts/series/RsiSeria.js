785bff8027f66c4c58beeba16c274e6ea2099acf
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-15 12:14:03 +0300

    Add useMemo to RsiSeria function component

--- js/components/charts/series/RsiSeria.js
+++ js/components/charts/series/RsiSeria.js
@@ -5,6 +5,8 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
+var _uiApi = require("../../uiApi");
+
 var _Ch = _interopRequireDefault(require("../Ch"));
 
 var _chartFns = require("../chartFns");
@@ -12,9 +14,6 @@ var _chartFns = require("../chartFns");
 var _jsxRuntime = require("react/jsx-runtime");
 
 var CL_TOOLTIP = 'rs-tooltip';
-
-var FN_NOOP = function FN_NOOP() {};
-
 var _rsiStroke = {
   line: '#000000',
   top: '#b8b2bb',
@@ -24,42 +23,52 @@ var _rsiStroke = {
   insideThreshold: '#4699cb'
 };
 
+var CHART_Y_EXTENDS = [0, 100],
+    CHART_ORIGIN = function CHART_ORIGIN(w, h) {
+  return [0, h - 510];
+},
+    YAXIS_TICK_VALUES = [30, 50, 70],
+    RSI_Y_ACCESOR = function RSI_Y_ACCESOR(d) {
+  return d.rsi;
+};
+
 var RsiSeria = function RsiSeria(_ref) {
   var id = _ref.id,
       height = _ref.height,
       width = _ref.width,
       rsi = _ref.rsi;
+
+  var _rsiTooltipOrigin = (0, _uiApi.useMemo)(function () {
+    return [width - 160, 10];
+  }, [width]),
+      _rsiOptions = (0, _uiApi.useMemo)(function () {
+    return rsi.options();
+  }, [rsi]);
+
   return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_Ch["default"].Chart, {
     id: id,
     height: height,
-    yExtents: [0, 100],
-    origin: function origin(w, h) {
-      return [0, h - 510];
-    },
+    yExtents: CHART_Y_EXTENDS,
+    origin: CHART_ORIGIN,
     children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_Ch["default"].YAxis, {
       axisAt: "right",
       orient: "right",
       stroke: "black",
       tickStroke: "#4699cb",
-      tickValues: [30, 50, 70]
+      tickValues: YAXIS_TICK_VALUES
     }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Ch["default"].MouseCoordinateY, {
       at: "right",
       orient: "right",
       displayFormat: _chartFns.numberFormat2F
     }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Ch["default"].RSISeries, {
-      yAccessor: function yAccessor(d) {
-        return d.rsi;
-      },
+      yAccessor: RSI_Y_ACCESOR,
       stroke: _rsiStroke
     }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Ch["default"].RSITooltip, {
       className: CL_TOOLTIP,
-      origin: [width - 160, 10],
       fontSize: 15,
-      yAccessor: function yAccessor(d) {
-        return d.rsi;
-      },
-      options: rsi.options(),
-      onClick: FN_NOOP
+      yAccessor: RSI_Y_ACCESOR,
+      origin: _rsiTooltipOrigin,
+      options: _rsiOptions
     })]
   });
 };
--- src/components/charts/series/RsiSeria.jsx
+++ src/components/charts/series/RsiSeria.jsx
@@ -1,9 +1,9 @@
+import { useMemo } from '../../uiApi';
+
 import Ch from '../Ch';
 import { numberFormat2F } from '../chartFns';
 
 const CL_TOOLTIP = 'rs-tooltip';
-
-const FN_NOOP = () => {}
 
 const _rsiStroke = {
   line: '#000000',
@@ -14,43 +14,58 @@ const _rsiStroke = {
   insideThreshold: '#4699cb'
 };
 
+const CHART_Y_EXTENDS = [0, 100]
+, CHART_ORIGIN = (w, h) => [0, h - 510]
+, YAXIS_TICK_VALUES = [30, 50, 70]
+, RSI_Y_ACCESOR = d => d.rsi;
+
 const RsiSeria = ({
   id,
   height,
   width,
   rsi
-}) => (
-  <Ch.Chart
-    id={id}
-    height={height}
-    yExtents={[0, 100]}
-    origin={(w, h) => [0, h - 510]}
-  >
-    <Ch.YAxis
-      axisAt="right"
-      orient="right"
-      stroke="black"
-      tickStroke="#4699cb"
-      tickValues={[30, 50, 70]}
-    />
-    <Ch.MouseCoordinateY
-      at="right"
-      orient="right"
-      displayFormat={numberFormat2F}
-    />
-    <Ch.RSISeries
-      yAccessor={d => d.rsi}
-      stroke={_rsiStroke}
-    />
-    <Ch.RSITooltip
-      className={CL_TOOLTIP}
-      origin={[width-160, 10]}
-      fontSize={15}
-      yAccessor={d => d.rsi}
-      options={rsi.options()}
-      onClick={FN_NOOP}
-    />
-  </Ch.Chart>
-);
+}) => {
+  const _rsiTooltipOrigin = useMemo(
+    ()=>[width-160, 10],
+    [width]
+  ),
+  _rsiOptions = useMemo(
+    () => rsi.options(),
+    [rsi]
+  );
+
+  return (
+    <Ch.Chart
+      id={id}
+      height={height}
+      yExtents={CHART_Y_EXTENDS}
+      origin={CHART_ORIGIN}
+    >
+      <Ch.YAxis
+        axisAt="right"
+        orient="right"
+        stroke="black"
+        tickStroke="#4699cb"
+        tickValues={YAXIS_TICK_VALUES}
+      />
+      <Ch.MouseCoordinateY
+        at="right"
+        orient="right"
+        displayFormat={numberFormat2F}
+      />
+      <Ch.RSISeries
+        yAccessor={RSI_Y_ACCESOR}
+        stroke={_rsiStroke}
+      />
+      <Ch.RSITooltip
+        className={CL_TOOLTIP}
+        fontSize={15}
+        yAccessor={RSI_Y_ACCESOR}
+        origin={_rsiTooltipOrigin}
+        options={_rsiOptions}
+      />
+    </Ch.Chart>
+  );
+}
 
 export default RsiSeria
