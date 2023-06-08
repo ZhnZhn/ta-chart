5cb63499954f4f05cd8e924e2bc54221aee325dd
Yevhenii Molodyi <eugmolody@gmail.com>
2023-06-08 17:57:57 +0300

    Edit chart crMouseCoordinate factory

--- js/components/zhn-charts/coordinates/crMouseCoordinate.js
+++ js/components/zhn-charts/coordinates/crMouseCoordinate.js
@@ -1,27 +1,26 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports["default"] = void 0;
-
 var _GenericChartComponent = _interopRequireDefault(require("../core/GenericChartComponent"));
-
 var _contextFn = require("../core/contextFn");
-
-var _useRenderSvg = _interopRequireDefault(require("./useRenderSvg"));
-
-var _useDrawOnCanvas = _interopRequireDefault(require("./useDrawOnCanvas"));
-
+var _EdgeCoordinateV = require("./EdgeCoordinateV3");
 var _jsxRuntime = require("react/jsx-runtime");
-
 var DRAWN_ON = ['mousemove', 'pan', 'drag'];
-
 var crMouseCoordinate = function crMouseCoordinate(crCoordinateProps) {
   return function (props) {
-    var _renderSVG = (0, _useRenderSvg["default"])(props, crCoordinateProps),
-        _drawOnCanvas = (0, _useDrawOnCanvas["default"])(props, crCoordinateProps);
-
+    var _renderSVG = function _renderSVG(moreProps) {
+        var _props = crCoordinateProps(props, moreProps);
+        return _props == null ? null : (0, _EdgeCoordinateV.renderSVG)(_props);
+      },
+      _drawOnCanvas = function _drawOnCanvas(ctx, moreProps) {
+        var _props = crCoordinateProps(props, moreProps);
+        if (_props == null) {
+          return null;
+        }
+        (0, _EdgeCoordinateV.drawOnCanvas)(ctx, _props);
+      };
     return /*#__PURE__*/(0, _jsxRuntime.jsx)(_GenericChartComponent["default"], {
       clip: false,
       svgDraw: _renderSVG,
@@ -31,7 +30,6 @@ var crMouseCoordinate = function crMouseCoordinate(crCoordinateProps) {
     });
   };
 };
-
 var _default = crMouseCoordinate;
 exports["default"] = _default;
 //# sourceMappingURL=crMouseCoordinate.js.map
--- src/components/zhn-charts/coordinates/crMouseCoordinate.js
+++ src/components/zhn-charts/coordinates/crMouseCoordinate.js
@@ -1,23 +1,35 @@
 import GenericChartComponent from '../core/GenericChartComponent';
-import {
-  getMouseCanvas
-} from '../core/contextFn';
+import { getMouseCanvas } from '../core/contextFn';
 
-import useRenderSvg from './useRenderSvg';
-import useDrawOnCanvas from './useDrawOnCanvas';
+import { renderSVG } from './EdgeCoordinateV3';
+import { drawOnCanvas } from './EdgeCoordinateV3';
 
 const DRAWN_ON = ['mousemove', 'pan', 'drag'];
 
-const crMouseCoordinate = crCoordinateProps => props => {
-  const _renderSVG = useRenderSvg(props, crCoordinateProps)
-  , _drawOnCanvas = useDrawOnCanvas(props, crCoordinateProps);
+const crMouseCoordinate = (
+  crCoordinateProps
+) => props => {
+  const _renderSVG = (moreProps) => {
+     const _props = crCoordinateProps(props, moreProps);
+     return _props == null
+       ? null
+       : renderSVG(_props);
+  }
+  , _drawOnCanvas = (ctx, moreProps) => {
+     const _props = crCoordinateProps(props, moreProps);
+     if (_props == null) {
+       return null;
+     }
+     drawOnCanvas(ctx, _props);
+  };
+
   return (
     <GenericChartComponent
-			clip={false}
-			svgDraw={_renderSVG}
-			canvasDraw={_drawOnCanvas}
-			canvasToDraw={getMouseCanvas}
-			drawOn={DRAWN_ON}
+			 clip={false}
+			 svgDraw={_renderSVG}
+			 canvasDraw={_drawOnCanvas}
+			 canvasToDraw={getMouseCanvas}
+			 drawOn={DRAWN_ON}
 		/>
   );
 }
