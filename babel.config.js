dba42af192a7295e98ce6ef59b96dbd266e44e4b
Yevhenii Molodyi <eugmolody@gmail.com>
2023-06-09 14:47:37 +0300

    Add babel browsers targets

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { loose: true }],
+    ['@babel/preset-env', { 
+       loose: true,
+       useBuiltIns: "entry",
+       corejs: "3",      
+       targets: {
+         browsers: [
+            ">0.25%", 
+            /* support arrow-functions */
+            "not ie 11", 
+            "not op_mini all", 
+            "not android 4.4.3-4.4.4", 
+            "not dead"
+         ] 
+       },
+       exclude: []      
+    }],    
     ['@babel/preset-react', { runtime: "automatic" }]
   ],
   plugins: [
