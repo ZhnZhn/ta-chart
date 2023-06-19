0700c3f1671dbfb1573eee2fd07bf36b5c393198
Yevhenii Molodyi <eugmolody@gmail.com>
2023-06-19 20:06:50 +0300

    Edit chart d3Time year

--- js/components/zhn-charts/d3Time/year.js
+++ js/components/zhn-charts/d3Time/year.js
@@ -3,6 +3,7 @@
 exports.__esModule = true;
 exports.utcYear = exports.timeYear = void 0;
 var _interval = require("./interval");
+const mathFloor = Math.floor;
 const timeYear = (0, _interval.timeInterval)(date => {
   date.setMonth(0, 1);
   date.setHours(0, 0, 0, 0);
@@ -12,15 +13,13 @@ const timeYear = (0, _interval.timeInterval)(date => {
 
 // An optimized implementation for this simple case.
 exports.timeYear = timeYear;
-timeYear.every = k => {
-  return !isFinite(k = Math.floor(k)) || !(k > 0) ? null : (0, _interval.timeInterval)(date => {
-    date.setFullYear(Math.floor(date.getFullYear() / k) * k);
-    date.setMonth(0, 1);
-    date.setHours(0, 0, 0, 0);
-  }, (date, step) => {
-    date.setFullYear(date.getFullYear() + step * k);
-  });
-};
+timeYear.every = k => !isFinite(k = mathFloor(k)) || !(k > 0) ? null : (0, _interval.timeInterval)(date => {
+  date.setFullYear(mathFloor(date.getFullYear() / k) * k);
+  date.setMonth(0, 1);
+  date.setHours(0, 0, 0, 0);
+}, (date, step) => {
+  date.setFullYear(date.getFullYear() + step * k);
+});
 const utcYear = (0, _interval.timeInterval)(date => {
   date.setUTCMonth(0, 1);
   date.setUTCHours(0, 0, 0, 0);
@@ -30,13 +29,11 @@ const utcYear = (0, _interval.timeInterval)(date => {
 
 // An optimized implementation for this simple case.
 exports.utcYear = utcYear;
-utcYear.every = k => {
-  return !isFinite(k = Math.floor(k)) || !(k > 0) ? null : (0, _interval.timeInterval)(date => {
-    date.setUTCFullYear(Math.floor(date.getUTCFullYear() / k) * k);
-    date.setUTCMonth(0, 1);
-    date.setUTCHours(0, 0, 0, 0);
-  }, (date, step) => {
-    date.setUTCFullYear(date.getUTCFullYear() + step * k);
-  });
-};
+utcYear.every = k => !isFinite(k = mathFloor(k)) || !(k > 0) ? null : (0, _interval.timeInterval)(date => {
+  date.setUTCFullYear(mathFloor(date.getUTCFullYear() / k) * k);
+  date.setUTCMonth(0, 1);
+  date.setUTCHours(0, 0, 0, 0);
+}, (date, step) => {
+  date.setUTCFullYear(date.getUTCFullYear() + step * k);
+});
 //# sourceMappingURL=year.js.map
