147384bf57c5792f55734af1f957115b99865f07
Yevhenii Molodyi <eugmolody@gmail.com>
2026-09-08 12:32:45 +0300

    Add rspack.prod.js

---
+++ rspack.prod.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const path = require('path')
+, HtmlWebpackPlugin = require('html-webpack-plugin')
+, babelConfig = require('./babel.config')
+, { rspack } = require('@rspack/core');
+
+
+module.exports = {
+  mode: "production",
+  cache: true,
+  entry: {
+    app: {
+      import: path.resolve('src', 'index.jsx'),
+      dependOn: 'lib'
+    },
+    lib: [
+       "react",
+       "react-dom",                           
+       "d3-quadtree"                                                                                                
+    ],
+  },
+  output: {
+      path: path.resolve('app'),
+      filename: "[name]_[chunkhash].js",
+      chunkFilename: "[name]_[chunkhash].js",
+      publicPath: 'app/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+             cacheDirectory: true,
+             ...babelConfig
+          }
+        },        
+        include: [
+          path.join(__dirname),
+          path.join(__dirname, "src"),
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx']    
+  },
+  plugins : [    
+    new HtmlWebpackPlugin({
+      minify: false,
+      filename: path.resolve('index.html'),
+      template: path.resolve('template', 'index.ejs'),
+      inject: false
+    })
+  ],
+  optimization: {        
+    minimize: true,    
+    minimizer: [
+       new rspack.SwcJsMinimizerRspackPlugin()
+    ]
+  },
+  stats: {    
+    colors: true
+  }
+}
