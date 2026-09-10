b147790a98f857a75b9203a82b5ff94b47c2abbc
Yevhenii Molodyi <eugmolody@gmail.com>
2026-09-10 12:12:50 +0300

    Format webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,31 @@
-'use strict'
+"use strict"
 
-const path = require('path')
-    , webpack = require('webpack')
-    //, { BundleStatsWebpackPlugin } = require('bundle-stats-webpack-plugin')  
-    , HtmlWebpackPlugin = require('html-webpack-plugin')
-    , babelConfig = require('./babel.config')
-    , TerserPlugin = require('terser-webpack-plugin');
+const path = require("path")
+, webpack = require("webpack")
+//, { BundleStatsWebpackPlugin } = require("bundle-stats-webpack-plugin")  
+, HtmlWebpackPlugin = require("html-webpack-plugin")
+, babelConfig = require("./babel.config")
+, TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   mode: "production",
   cache: true,
   entry: {
-    app: {
-      import: path.resolve('src', 'index.jsx'),
-      dependOn: 'lib'
-    },
     lib: [           
-            "react",
-            "react-dom",                           
-            "d3-quadtree"                                                                                        
+      "react",
+      "react-dom",                           
+      "d3-quadtree"                                                                                        
     ],
+    app: {
+      import: path.resolve("src", "index.jsx"),
+      dependOn: "lib"
+    }    
   },
   output: {
-      path: path.resolve('app'),
+      path: path.resolve("app"),
       filename: "[name]_[chunkhash].js",
       chunkFilename: "[name]_[chunkhash].js",
-      publicPath: 'app/'
+      publicPath: "app/"
   },
   module: {
     rules: [
@@ -33,7 +33,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
              cacheDirectory: true,
              ...babelConfig
@@ -47,15 +47,15 @@ module.exports = {
     ]
   },
   resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.jsx']
+    modules: ["node_modules"],
+    extensions: [".js", ".jsx"]
   },
   plugins : [
     //new BundleStatsWebpackPlugin(),
     new HtmlWebpackPlugin({
       minify: false,
-      filename: path.resolve('index.html'),
-      template: path.resolve('template', 'index.ejs'),
+      filename: path.resolve("index.html"),
+      template: path.resolve("template", "index.ejs"),
       inject: false
     })
   ],
